ce2e9d4fd22a78bfbc72f85ff504bd97f2911e86
IshaqKassam <ishaq.kassam@gmail.com>
2022-09-11 16:29:27 +0300

    changed to https

--- src/utilities.js
+++ src/utilities.js
@@ -47,7 +47,7 @@ export const drawRect = (video, canvas, detections, ctx) => {
 			payload.append("file", file, "image.jpeg")
 
 			try {
-				fetch("http://127.0.0.1:5000/predict", {
+				fetch("https://127.0.0.1:5000/predict", {
 					method: "POST",
 					body: payload,
 				}).then((response) => {
